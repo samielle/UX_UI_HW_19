565ae01dc52c592abf45f90ea09daf1786511fa7
Samara Leung <samara@maravisuals.com>
2021-11-26 11:29:43 +1000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
 
 console.log("Your index.js file is loaded correctly!");
 
-$("button").on({
+$('button').on({
     mouseenter: function () {
-        $(this).css("background-color", "#F7E0E4");
+        $(this).css('background-color', '#F7E0E4');
     },
     mouseleave: function () {
-        $(this).css("background-color", "#F1C6CD");
+        $(this).css('background-color', '#F1C6CD');
     },
     click: function () {
-        $(this).css("background-color", "#CDA8AE");
+        $(this).css('background-color', '#CDA8AE');
+    }
+});
+
+$('button').on({
+    mouseenter: function () {
+        $(this).css('color', '#91777B');
+    },
+    mouseleave: function () {
+        $(this).css('color', 'black');
     }
 });
